74935f8ae511934c6c13ad45a6407bbceb3d4a80
Ralph Bean <ralph.bean@gmail.com>
2011-09-07 10:01:59 -0400

    Pull long desc from docstring.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 import sys, os
 
+import brutius_slurmimus
+long_description = brutius_slurmimus.__doc__
+
 version = '0.1'
 
 setup(name='brutius_slurmimus',
       version=version,
       description="Parse-and-return slurm commands.  Don't use this.  Use pyslurm instead.",
-      long_description="""\
-""",
+      long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Ralph Bean',
