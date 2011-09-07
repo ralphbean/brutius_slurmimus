423f3d06cf95b8fa21d8a6511a5ce035d81d5b87
Ralph Bean <ralph.bean@gmail.com>
2011-09-07 09:41:28 -0400

    Paster create output.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='brutius_slurmimus',
+      version=version,
+      description="Parse-and-return slurm commands.  Don't use this.  Use pyslurm instead.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ralph Bean <ralph.bean@gmail.com',
+      author_email='ralph.bean@gmail.com',
+      url='http://github.com/ralphbean/brutius_slurmimus',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
